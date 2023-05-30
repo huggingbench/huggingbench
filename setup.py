496f2558d2575766f8a7465d25d027981553af12
kiarash <kiarash.rezahanjani@gmail.com>
2023-05-30 11:22:15 +0200

    Add all dep to setup py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-# hugging_bench/setup.py
-
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name='hugging_bench',
+    name='hugging-bench-project',
     version='1.0',
     packages=find_packages(),
-    # install_requires=[
-    #     'client',  # Specify the client package as a dependency
-    # ],
-    install_requires=requirements,
+    install_requires=[
+        # install rust compier as need for building some packages in wheel   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        # and export PATH="$HOME/.cargo/bin:$PATH"
+        'tritonclient[all]==2.33.0',
+        'prometheus-client==0.16.0',
+        'docker==6.1.2',
+        'datasets==2.12.0',
+        'transformers==4.11.3',
+        'numpy',
+    ]
 )
