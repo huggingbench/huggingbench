0547f0fda061c9940cc354f5f4b78fafc0297a40
kiarash <kiarash.rezahanjani@gmail.com>
2023-06-01 21:07:24 +0200

    add test for modelinfo

--- test/test_hugging_bench_config.py
+++ test/test_hugging_bench_config.py
@@ -2,7 +2,7 @@ import pytest
 from hugging_bench.hugging_bench_config import ModelInfo, Format, Input, Output
 
 @pytest.fixture
-def sample_model_info():
+def onnx_model_info():
     format_params = {'device': 'cuda', 'half': True}
     format = Format(format_type='onnx', parameters=format_params)
     input_shape = [Input(name='input', dtype='FP32', dims=[1, 3, 224, 224])]
@@ -10,32 +10,70 @@ def sample_model_info():
     return ModelInfo(hf_id='hf_model_id', task='classification', format=format,
                      base_dir='/path/to/models', input_shape=input_shape, output_shape=output_shape)
 
-def test_unique_name(sample_model_info):
-    assert sample_model_info.unique_name() == 'hf_model_id-classification-onnx-True-cuda'
+def test_unique_name(onnx_model_info):
+    assert onnx_model_info.unique_name() == 'hf_model_id-classification-onnx-True-cuda'
 
-def test_model_dir(sample_model_info):
-    assert sample_model_info.model_dir() == '/path/to/models/hf_model_id-classification-onnx-True-cuda'
+def test_model_dir(onnx_model_info):
+    assert onnx_model_info.model_dir() == '/path/to/models/hf_model_id-classification-onnx-True-cuda'
 
-def test_model_file_path(sample_model_info):
-    assert sample_model_info.model_file_path() == '/path/to/models/hf_model_id-classification-onnx-True-cuda/model.onnx'
+def test_model_file_path(onnx_model_info):
+    assert onnx_model_info.model_file_path() == '/path/to/models/hf_model_id-classification-onnx-True-cuda/model.onnx'
 
-def test_gpu_enabled(sample_model_info):
-    assert sample_model_info.gpu_enabled() == True
+def test_gpu_enabled(onnx_model_info):
+    assert onnx_model_info.gpu_enabled() == True
 
-def test_half(sample_model_info):
-    assert sample_model_info.half() == True
+def test_half(onnx_model_info):
+    assert onnx_model_info.half() == True
 
-def test_with_shapes(sample_model_info):
+def test_with_shapes(onnx_model_info):
     input_shape = [Input(name='input', dtype='FP32', dims=[1, 3, 224, 224])]
     output_shape = [Output(name='output', dtype='FP32', dims=[1, 1000])]
-    new_model_info = sample_model_info.with_shapes(input_shape, output_shape)
+    new_model_info = onnx_model_info.with_shapes(input_shape, output_shape)
     assert new_model_info.input_shape == input_shape
     assert new_model_info.output_shape == output_shape
 
-def test_tags(sample_model_info):
-    tags = sample_model_info.tags()
+def test_tags(onnx_model_info):
+    tags = onnx_model_info.tags()
     assert tags['hf_id'] == 'hf_model_id'
     assert tags['task'] == 'classification'
     assert tags['format'] == 'onnx'
     assert tags['gpu'] == 'True'
     assert tags['half'] == 'True'
+
+
+
+@pytest.fixture
+def trt_model_info():
+    format_params = {'device': 'cuda', 'half': False}
+    format_origin = Format(format_type='onnx', parameters=format_params)
+    format_trt = Format(format_type='trt', origin=format_origin)
+    input_shape = [Input(name='input', dtype='float32', dims=[1, 3, 224, 224])]
+    output_shape = [Output(name='output', dtype='float32', dims=[1, 1000])]
+    return ModelInfo(hf_id='model_id', task='classification', format=format_trt, base_dir='/path/to/model', input_shape=input_shape, output_shape=output_shape)
+
+def test_trt_unique_name(trt_model_info):
+    assert trt_model_info.unique_name() == 'model_id-classification-trt-False-cuda'
+
+def test_trt_model_file_path(trt_model_info):
+    assert trt_model_info.model_file_path() == '/path/to/model/model_id-classification-trt-False-cuda/model.plan'
+
+def test_trt_gpu_enabled(trt_model_info):
+    assert trt_model_info.gpu_enabled() == True
+
+def test_trt_half(trt_model_info):
+    assert trt_model_info.half() == False
+
+def test_trt_with_shapes(trt_model_info):
+    input_shape = [Input(name='input', dtype='float32', dims=[1, 3, 224, 224])]
+    output_shape = [Output(name='output', dtype='float32', dims=[1, 1000])]
+    new_model_info = trt_model_info.with_shapes(input_shape, output_shape)
+    assert new_model_info.input_shape == input_shape
+    assert new_model_info.output_shape == output_shape
+
+def test_trt_tags(trt_model_info):
+    tags = trt_model_info.tags()
+    assert tags['hf_id'] == 'model_id'
+    assert tags['task'] == 'classification'
+    assert tags['format'] == 'trt'
+    assert tags['gpu'] == 'True'
+    assert tags['half'] == 'False'
