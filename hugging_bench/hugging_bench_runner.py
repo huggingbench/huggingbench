54211b258ec6c889f1bc2828ef868d4ef8ad8585
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-05 12:12:31 +0200

    Move running code to main to keep runner usable in e2e tests

--- hugging_bench/hugging_bench_runner.py
+++ hugging_bench/hugging_bench_runner.py
@@ -5,7 +5,7 @@ from hugging_bench.hugging_bench_config import ExperimentSpec, TritonServerSpec,
 from hugging_bench.hugging_bench_triton import TritonConfig, TritonServer
 from client.triton_client import TritonClient
 from client.runner import RunnerConfig, Runner
-from client.datasets import get_dataset
+
 import os
 
 class ExperimentRunner:
@@ -57,38 +57,6 @@ class ExperimentRunner:
 
     
 
-experiments=[ 
-    # ExperimentSpec(format="trt", device="cuda", half=True),
-    # ExperimentSpec(format="trt", device="cuda", half=False),
-# 
-    # ExperimentSpec(format="onnx", device="cuda", half=True),   
-    # ExperimentSpec(format="onnx", device="cuda", half=False),
-    ExperimentSpec(format="onnx", device="cpu", half=False),
-]
-
-server_spec = TritonServerSpec()
-
-# cover all models with random data
-
-ExperimentRunner("microsoft/resnet-50", experiments, server_spec, dataset=None, model_local_path="/Users/niksa/projects/models/resnet-50").run()
-# ExperimentRunner("bert-base-uncased", experiments, server_spec, dataset=None, model_local_path="/Users/niksa/.cache/huggingface/hub/models--bert-base-uncased/snapshots/0a6aa9128b6194f4f3c4db429b6cb4891cdb421b", task="text-classification").run()
-
-# ExperimentRunner("microsoft/resnet-50", experiments, server_spec, dataset=None).run()
-# ExperimentRunner("bert-base-uncased", experiments, server_spec, dataset=None).run()
-
-# ExperimentRunner("distilbert-base-uncased", experiments, server_spec, dataset=None).run()
-# ExperimentRunner("facebook/bart-large", experiments, server_spec, dataset=None, task='feature-extraction').run()
 
 
-# resnet50_gen_dataset = get_dataset("microsoft/resnet-50-gen")
-# bert_gen_dataset = get_dataset("bert-base-uncased-gen")
-# ExperimentRunner("microsoft/resnet-50", resnet50_gen_dataset, experiments, server_spec).run()
-# ExperimentRunner("bert-base-uncased", bert_gen_dataset, experiments, server_spec).run()
 
-# ExperimentRunner("xlm-roberta-base", experiments, server_spec, dataset=None).run()
-
-# ExperimentRunner("bert-base-uncased", experiments, server_spec).run()
-# ExperimentRunner("distilbert-base-uncased", experiments, server_spec).run()
-# ExperimentRunner("microsoft/resnet-50", experiments, server_spec).run()
-# ExperimentRunner("bert-base-uncased", "./kiarash_server/model_repository", experiments).run()
-# ExperimentRunner("distilbert-base-uncased", "./kiarash_server/model_repository", experiments).run()
---
+++ hugging_bench/main.py
@@ -0,0 +1,40 @@
+from hugging_bench.hugging_bench_config import ExperimentSpec, TritonServerSpec
+from hugging_bench.hugging_bench_runner import ExperimentRunner
+from client.datasets import get_dataset
+
+def main():
+    experiments=[ 
+        # ExperimentSpec(format="trt", device="cuda", half=True),
+        # ExperimentSpec(format="trt", device="cuda", half=False),
+        # ExperimentSpec(format="onnx", device="cuda", half=True),   
+        # ExperimentSpec(format="onnx", device="cuda", half=False),
+        ExperimentSpec(format="onnx", device="cpu", half=False),
+        # ExperimentSpec(format="onnx", device="cpu", half=True)
+    ]
+    server_spec = TritonServerSpec()
+
+    ExperimentRunner("microsoft/resnet-50", experiments, server_spec, dataset=None, model_local_path="/Users/niksa/projects/models/resnet-50").run()
+    # ExperimentRunner("bert-base-uncased", experiments, server_spec, dataset=None, model_local_path="/Users/niksa/.cache/huggingface/hub/models--bert-base-uncased/snapshots/0a6aa9128b6194f4f3c4db429b6cb4891cdb421b", task="text-classification").run()
+
+    # ExperimentRunner("microsoft/resnet-50", experiments, server_spec, dataset=None).run()
+    # ExperimentRunner("bert-base-uncased", experiments, server_spec, dataset=None).run()
+
+    # ExperimentRunner("distilbert-base-uncased", experiments, server_spec, dataset=None).run()
+    # ExperimentRunner("facebook/bart-large", experiments, server_spec, dataset=None, task='feature-extraction').run()
+
+    # resnet50_gen_dataset = get_dataset("microsoft/resnet-50-gen")
+    # bert_gen_dataset = get_dataset("bert-base-uncased-gen")
+    # ExperimentRunner("microsoft/resnet-50", resnet50_gen_dataset, experiments, server_spec).run()
+    # ExperimentRunner("bert-base-uncased", bert_gen_dataset, experiments, server_spec).run()
+
+    # ExperimentRunner("xlm-roberta-base", experiments, server_spec, dataset=None).run()
+
+    # ExperimentRunner("bert-base-uncased", experiments, server_spec).run()
+    # ExperimentRunner("distilbert-base-uncased", experiments, server_spec).run()
+    # ExperimentRunner("microsoft/resnet-50", experiments, server_spec).run()
+    # ExperimentRunner("bert-base-uncased", "./kiarash_server/model_repository", experiments).run()
+    # ExperimentRunner("distilbert-base-uncased", "./kiarash_server/model_repository", experiments).run()
+
+
+if __name__ == "__main__":
+    main()
