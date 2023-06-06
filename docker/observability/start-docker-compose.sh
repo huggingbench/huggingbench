a22f86686215a25a179adea4dc7cf14b93ab8dc4
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-06 11:29:33 +0200

    Fix docker compose script for Mac

--- docker/observability/start-docker-compose.sh
+++ docker/observability/start-docker-compose.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # On Mac we have host.docker.internal set but on Linux we need to set it manually.
-# If running Linux use this script to spin up docker containers with Docker compose.
+# Use this script to spin up docker containers with Docker compose.
 
-export DOCKER_HOST_IP=$(ip addr show | grep "\binet\b.*\bdocker0\b" | awk '{print $2}' | cut -d '/' -f 1)
-docker-compose up -d
+
+if [ "Linux" == $(uname -s) ]; then
+    export DOCKER_HOST_IP=$(ip addr show | grep "\binet\b.*\bdocker0\b" | awk '{print $2}' | cut -d '/' -f 1)
+fi
+
+docker compose up -d
