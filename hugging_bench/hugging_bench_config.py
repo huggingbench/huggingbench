402f8f870cd98d716fea0e4f3fe9e77228056113
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-06 22:15:43 +0200

    Add batch_size into Prometheus tags

--- hugging_bench/hugging_bench_config.py
+++ hugging_bench/hugging_bench_config.py
@@ -7,6 +7,7 @@ import os
 TEMP_DIR = "./temp"
 TEMP_MODEL_REPO_DIR = f"{TEMP_DIR}/model_repository"
 
+
 @dataclass
 class TritonServerSpec:
     # TODO ports not used yet
@@ -23,7 +24,7 @@ class ExperimentSpec:
 
 @dataclass
 class Format:
-    format_type: str # onnx, openvino, torchscript, tensorflow
+    format_type: str  # onnx, openvino, torchscript, tensorflow
     parameters: dict = field(default_factory=dict)
     origin: 'Format' = None
 
@@ -31,67 +32,71 @@ class Format:
         if self.parameters.get('device') == 'cuda' or (self.origin and self.origin.parameters.get('device') == 'cuda'):
             return True
         return False
-    
+
     def half(self):
         if self.parameters.get('half', False) or (self.origin and self.origin.parameters.get('half', False)):
             return True
         return False
+
 
 @dataclass
 class Input:
     name: str
     dtype: str
     dims: list[int]
-    
+
+
 @dataclass
 class Output:
     name: str
     dtype: str
     dims: list[int]
- 
+
+
 @dataclass
 class ModelInfo:
     hf_id: str
     task: str
     format: Format
     base_dir: str
-    input_shape: list[Input] =  field(default_factory=list)  
+    input_shape: list[Input] = field(default_factory=list)
     output_shape: list[Output] = field(default_factory=list)
 
     def unique_name(self):
-        params_str = f"-{self.param_str()}" if(self.param_str()) else ""
+        params_str = f"-{self.param_str()}" if (self.param_str()) else ""
         return f"{self.hf_id}-{self.task}-{self.format.format_type}{params_str}".replace("/", "-")
-    
+
     def model_dir(self):
         return os.path.join(os.path.abspath(self.base_dir), self.unique_name())
-    
+
     def model_file_path(self):
-        if(self.format.format_type == "onnx"):
+        if (self.format.format_type == "onnx"):
             return os.path.join(self.model_dir(), "model.onnx")
-        elif(self.format.format_type == "openvino"):
+        elif (self.format.format_type == "openvino"):
             return os.path.join(self.model_dir(), "model.xml"), os.path.join(self.model_dir(), "model.bin")
-        elif(self.format.format_type == "trt"):
+        elif (self.format.format_type == "trt"):
             return os.path.join(self.model_dir(), "model.plan")
-        else: 
+        else:
             raise Exception("Model format is not onnx")
-        
-    
+
     def param_str(self):
-        format_params = '-'.join(sorted(map(str, self.format.parameters.values())))
+        format_params = '-'.join(sorted(map(str,
+                                 self.format.parameters.values())))
         if self.format.origin:
-            origin_params = '-'.join(sorted(map(str, self.format.origin.parameters.values())))
+            origin_params = '-'.join(sorted(map(str,
+                                     self.format.origin.parameters.values())))
             format_params += origin_params
         return format_params
-    
+
     def gpu_enabled(self):
         return self.format.gpu_enabled()
-    
+
     def half(self):
         return self.format.half()
-    
+
     def with_shapes(self, input_shape, output_shape):
         return self.__class__(self.hf_id, self.task, self.format, self.base_dir, input_shape, output_shape)
-    
+
     def tags(self):
         return {
             'hf_id': str(self.hf_id),
@@ -99,6 +104,5 @@ class ModelInfo:
             'format': self.format.format_type,
             'gpu': str(self.gpu_enabled()),
             'half': str(self.half()),
+            'batch_size': str(self.format.parameters.get('batch_size', 1)),
         }
-
-
--- hugging_bench/hugging_bench_util.py
+++ hugging_bench/hugging_bench_util.py
@@ -1,3 +1,9 @@
+import csv
+from dataclasses import replace
+from typing import NamedTuple, Dict
+from polygraphy.backend.onnx.util import get_input_metadata, get_output_metadata
+import onnx
+from hugging_bench.hugging_bench_config import Input, Output
 import os
 import os
 import subprocess
@@ -16,43 +22,47 @@ PRINT_HEADER = "\n\n============================%s==============================
 ENV_TRITON_SERVER_DOCKER = "triton_server_docker_image"
 
 LOG = logging.getLogger(__name__)
+
+
 class ModelExporter:
     # export model to onnx, openvino, trt...
-    
+
     def __init__(self, hf_id, spec: ExperimentSpec, task=None, base_dir=None) -> None:
         self.hf_id = hf_id
         self.spec = spec
         self.task = task
-        self.base_dir = base_dir if(base_dir) else os.getcwd()
-        
+        self.base_dir = base_dir if (base_dir) else os.getcwd()
+
     def export(self, model_input_path: str = None) -> ModelInfo:
-        #  onnx format is a starting point    
-        model_info = self._export_hf2onnx("0.001", self.spec.device, self.spec.half, model_input_path)
+        #  onnx format is a starting point
+        model_info = self._export_hf2onnx(
+            "0.001", self.spec.device, self.spec.half, model_input_path, self.spec.batch_size)
         model_info = model_info.with_shapes(
-                    input_shape=hf_model_input(model_info.model_file_path(), half=model_info.half()), 
-                    output_shape=hf_model_output(model_info.model_file_path(), half=model_info.half()))  
-        
-        
-        if(self.spec.format == "onnx"):   
+            input_shape=hf_model_input(
+                model_info.model_file_path(), half=model_info.half()),
+            output_shape=hf_model_output(model_info.model_file_path(), half=model_info.half()))
+
+        if (self.spec.format == "onnx"):
             None
-        elif(self.spec.format == "openvino"):
+        elif (self.spec.format == "openvino"):
             model_info = self._export_onnx2openvino(model_info)
-        elif(self.spec.format == "trt"):
+        elif (self.spec.format == "trt"):
             model_info = self._export_onnx2trt(model_info)
         else:
             raise Exception(f"Unknown format {self.spec.format}")
-        
+
         LOG.info(f"Model info {model_info}")
         return model_info
 
-    def _export_hf2onnx(self, atol=0.001, device=None, half=False, model_input:str = None) -> ModelInfo:
+    def _export_hf2onnx(self, atol=0.001, device=None, half=False, model_input: str = None, batch_size: int = 1) -> ModelInfo:
         print(PRINT_HEADER % " ONNX EXPORT ")
-        model_info = ModelInfo(self.hf_id, self.task, Format("onnx", {"atol": atol, "device": device, "half": half}), base_dir=self.base_dir)
-        
-        if(all(os.path.exists(file) for file in model_info.model_file_path())): 
+        model_info = ModelInfo(self.hf_id, self.task, Format(
+            "onnx", {"atol": atol, "device": device, "half": half, "batch_size": batch_size}), base_dir=self.base_dir)
+
+        if (all(os.path.exists(file) for file in model_info.model_file_path())):
             LOG.info(f"Model already exists at {model_info.model_file_path()}")
             return model_info
-        
+
         model_dir = model_info.model_dir()
         os.makedirs(model_dir, exist_ok=True)
 
@@ -60,42 +70,40 @@ class ModelExporter:
 
         cmd = [
             "optimum-cli", "export", "onnx",
-            model_arg, 
+            model_arg,
             "--framework=pt",
-            "--monolit", 
+            "--monolit",
             f"--atol={atol}"]
-        
-        if(not half and device):
+
+        if (not half and device):
             cmd.append(f"--device={device}")
-        
-        if(half):
+
+        if (half):
             cmd.append("--fp16")
             cmd.append("--device=cuda")
-        
-        if(self.task):
-            cmd.append(f"--task={self.task}")
-        
 
-        
+        if (self.task):
+            cmd.append(f"--task={self.task}")
+
         cmd.append(model_info.model_dir())
-        
-        run_docker_sdk("optimum", model_dir, cmd, model_info.gpu_enabled(), model_input=model_input)
-        
+
+        run_docker_sdk("optimum", model_dir, cmd,
+                       model_info.gpu_enabled(), model_input=model_input)
+
         return model_info
 
-
     def _export_onnx2openvino(self, onnx_model_info: ModelInfo):
-        LOG.info(PRINT_HEADER % " ONNX 2 OPENVINO CONVERSION ")   
+        LOG.info(PRINT_HEADER % " ONNX 2 OPENVINO CONVERSION ")
         ov_model_info = ModelInfo(
-            onnx_model_info.hf_id, 
-            onnx_model_info.task, 
-            format=Format("openvino", origin=onnx_model_info.format), 
-            base_dir=self.base_dir, 
+            onnx_model_info.hf_id,
+            onnx_model_info.task,
+            format=Format("openvino", origin=onnx_model_info.format),
+            base_dir=self.base_dir,
             input_shape=onnx_model_info.input_shape,
             output_shape=onnx_model_info.output_shape)
         model_dir = ov_model_info.model_dir()
         os.makedirs(model_dir, exist_ok=True)
-        
+
         cmd = [
             "mo",
             f"--input_model={onnx_model_info.model_file_path()[0]}",
@@ -103,22 +111,22 @@ class ModelExporter:
         ]
         run_docker_sdk(image_name="openvino", docker_args=cmd)
         return ov_model_info
-    
 
     def _export_onnx2trt(self, onnx_model_info):
-        LOG.info(PRINT_HEADER % " ONNX 2 TRT CONVERSION ") 
+        LOG.info(PRINT_HEADER % " ONNX 2 TRT CONVERSION ")
         trt_model_info = ModelInfo(
-            onnx_model_info.hf_id, 
-            onnx_model_info.task, 
-            Format("trt", origin=onnx_model_info.format), 
+            onnx_model_info.hf_id,
+            onnx_model_info.task,
+            Format("trt", origin=onnx_model_info.format),
             self.base_dir,
             input_shape=onnx_model_info.input_shape,
             output_shape=onnx_model_info.output_shape)
-        
+
         model_dir = trt_model_info.model_dir()
         os.makedirs(model_dir, exist_ok=True)
 
-        input_str = ' '.join([f"{input.name}:{input.dims}" for input in trt_model_info.input_shape])
+        input_str = ' '.join(
+            [f"{input.name}:{input.dims}" for input in trt_model_info.input_shape])
 
         cmd = [
             "polygraphy",
@@ -129,26 +137,27 @@ class ModelExporter:
             f"--output={trt_model_info.model_file_path()[0]}",
             onnx_model_info.model_file_path()[0]
         ]
-        run_docker_sdk(image_name="nvcr.io/nvidia/tensorrt:23.04-py3", docker_args=cmd, gpu=True)
+        run_docker_sdk(
+            image_name="nvcr.io/nvidia/tensorrt:23.04-py3", docker_args=cmd, gpu=True)
         return trt_model_info
-
 
     def _inspect_onnx(self, model_info: ModelInfo):
         LOG.info(PRINT_HEADER % " ONNX MODEL INSPECTION ")
-        run_docker_sdk(image_name="nvcr.io/nvidia/tensorrt:23.04-py3", docker_args=["polygraphy", "inspect", "model", f"{model_info.model_file_path()[0]}", "--mode=onnx"], env={"POLYGRAPHY_AUTOINSTALL_DEPS": 1})
-        
+        run_docker_sdk(image_name="nvcr.io/nvidia/tensorrt:23.04-py3", docker_args=[
+                       "polygraphy", "inspect", "model", f"{model_info.model_file_path()[0]}", "--mode=onnx"], env={"POLYGRAPHY_AUTOINSTALL_DEPS": 1})
+
 
 def dtype_np_type(dtype: str):
     from tritonclient.utils import triton_to_np_dtype
     from hugging_bench_triton import TritonConfig
     return triton_to_np_dtype(TritonConfig.DTYPE_MAP.get(dtype, None))
-    
+
 
 def run_docker(image_name, workspace=None, docker_args=[]):
     import shlex
 
     # Construct Docker command
-    if(not workspace):
+    if (not workspace):
         workspace = os.getcwd()
     command = f'docker run --gpus=all -v {workspace}:{workspace} -w {workspace}  {image_name} {" ".join(docker_args)}'
     try:
@@ -165,6 +174,7 @@ def run_docker(image_name, workspace=None, docker_args=[]):
     except Exception as e:
         raise e
 
+
 def print_container_logs(container, callback=None):
     """
     Prints logs of a Docker container until a specific message appears or a timeout is reached.
@@ -175,13 +185,11 @@ def print_container_logs(container, callback=None):
         callback(log_line) if callback else None
 
 
-
-
 def run_docker_sdk(image_name, workspace=None, docker_args=[], gpu=False, env={}, model_input=None):
     import docker
     client = docker.from_env()
 
-    if(not workspace):
+    if (not workspace):
         workspace = os.getcwd()
 
     volumes = {
@@ -189,29 +197,26 @@ def run_docker_sdk(image_name, workspace=None, docker_args=[], gpu=False, env={}
         model_input: {'bind': "/model_input", 'mode': 'rw'}
     }
 
-    LOG.info(f"Running Docker container {image_name} gpu: {gpu} with command: {docker_args} and volumes: {volumes}")
+    LOG.info(
+        f"Running Docker container {image_name} gpu: {gpu} with command: {docker_args} and volumes: {volumes}")
     container = client.containers.run(
         image_name,
         command=docker_args,
         volumes=volumes,
         device_requests=[
-                    docker.types.DeviceRequest(device_ids=["0"], capabilities=[['gpu']])] if gpu else [],
+            docker.types.DeviceRequest(device_ids=["0"], capabilities=[['gpu']])] if gpu else [],
         working_dir=workspace,
         detach=True,
         environment=env,
         auto_remove=True
-        )
-    
-    t = Thread(target = print_container_logs, args=[container])
+    )
+
+    t = Thread(target=print_container_logs, args=[container])
     t.start()
     exit_code = container.wait()
     LOG.info(f"Docker container exit code {exit_code}")
     return exit_code
 
-
-from hugging_bench.hugging_bench_config import Input, Output
-import onnx
-from polygraphy.backend.onnx.util import get_input_metadata, get_output_metadata
 
 SHAPE_MAP = {
     "batch_size": 1,
@@ -225,6 +230,7 @@ SHAPE_MAP = {
     "feature_size": 80,
 }
 
+
 def get_dim_value(dim):
     if isinstance(dim, int) or dim.isnumeric():
         return int(dim)
@@ -233,19 +239,22 @@ def get_dim_value(dim):
         raise ValueError(f"Dimension {dim} not found in SHAPE_MAP")
     return value
 
-from dataclasses import replace
+
 def half_fp32(input):
-    new_input = replace(input, dtype = "FP16" if input.dtype=="FP32" else input.dtype)
+    new_input = replace(input, dtype="FP16" if input.dtype ==
+                        "FP32" else input.dtype)
     return new_input
+
 
 def hf_model_input(onnx_model_path: str, half=False):
     onnx_model = onnx.load(onnx_model_path)
     input_metadata_dict = get_input_metadata(onnx_model.graph)
     inputs = []
     for input_name, input_metadata in input_metadata_dict.items():
-        dims = [get_dim_value(dim) for dim in input_metadata.shape if dim != "batch_size"]
+        dims = [get_dim_value(dim)
+                for dim in input_metadata.shape if dim != "batch_size"]
         dtype = str(input_metadata.dtype).upper()
-        dtype=format_dtype(dtype)
+        dtype = format_dtype(dtype)
         inputs.append(Input(name=input_name, dtype=dtype, dims=dims))
 
     return list(map(half_fp32, inputs)) if half else inputs
@@ -256,29 +265,29 @@ def hf_model_output(onnx_model_path: str, half=False):
     output_metadata_dict = get_output_metadata(onnx_model.graph)
     outputs = []
     for output_name, output_metadata in output_metadata_dict.items():
-        dims = [get_dim_value(dim) for dim in list(output_metadata.shape) if dim != "batch_size"]
+        dims = [get_dim_value(dim) for dim in list(
+            output_metadata.shape) if dim != "batch_size"]
         dtype = str(output_metadata.dtype).upper()
-        dtype=format_dtype(dtype)
+        dtype = format_dtype(dtype)
         outputs.append(Output(name=output_name, dtype=dtype, dims=dims))
-    
+
     return list(map(half_fp32, outputs)) if half else outputs
 
 
 def format_dtype(dtype):
-    if(dtype=='FLOAT32'):
+    if (dtype == 'FLOAT32'):
         return "FP32"
-    elif(dtype=='FLOAT16'):
+    elif (dtype == 'FLOAT16'):
         return "FP16"
     else:
         return dtype
 
-import csv
-from typing import NamedTuple, Dict
 
 class ExperimentSpec(NamedTuple):
     format: str
     device: str
     half: bool
+
 
 def append_to_csv(spec_dict: Dict, info: Dict, csv_file: str):
     """
