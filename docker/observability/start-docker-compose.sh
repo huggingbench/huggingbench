f1e4e1ed968b15bf39a06c744fcb5dc9a6e709da
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-06 11:14:20 +0200

    Make docker compose Linux compatible

---
+++ docker/observability/start-docker-compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# On Mac we have host.docker.internal set but on Linux we need to set it manually.
+# If running Linux use this script to spin up docker containers with Docker compose.
+
+export DOCKER_HOST_IP=$(ip addr show | grep "\binet\b.*\bdocker0\b" | awk '{print $2}' | cut -d '/' -f 1)
+docker-compose up -d
