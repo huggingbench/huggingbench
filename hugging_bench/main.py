804081b0bdda2c2be5bc88452b3dc760b573e966
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-14 13:55:02 +0200

    Pass few more command line arguments to runner

--- hugging_bench/main.py
+++ hugging_bench/main.py
@@ -10,9 +10,11 @@ def mlperf():
     # Define the command-line arguments with their default values
     parser.add_argument("--format", default="onnx", choices=["onnx", "trt", "openvino"])
     parser.add_argument("--device", default="cpu", choices=["cpu", "cuda"])
-    parser.add_argument("--half", default=False, type=bool)
-    parser.add_argument("--client_worker", default=1, type=int)
-    parser.add_argument("--hf_ids", default=["prajjwal1/bert-tiny"], nargs="*")
+    parser.add_argument("--half", default=False, type=bool, help="Whether to use half precision")
+    parser.add_argument("--client_worker", default=1, type=int, help="Number of client workers sending concurrent requests to Triton")
+    parser.add_argument("--hf_ids", default=["prajjwal1/bert-tiny"], nargs="*", help="HuggingFace model ID(s) to benchmark")
+    parser.add_argument("--model_local_path", default=None, nargs="*", help="If not specified, will download from HuggingFace. When given a task name must also be specified.")
+    parser.add_argument("--task", default=None, nargs="*", help="Model task(s) to benchmark. Used with --model_local_path")
 
     #  potential hf_ids: ["bert-base-uncased", "distilbert-base-uncased", "microsoft/resnet-5"]
 
@@ -25,11 +27,18 @@ def mlperf():
     half = args.half
     client_worker = args.client_worker
     hf_ids = args.hf_ids
+    model_local_path = args.model_local_path
+    task = args.task
 
-    for hf_id in hf_ids:
+    for idx, hf_id in enumerate(hf_ids):
         ExperimentRunner(
             hf_id,
             [ExperimentSpec(format=format_type, device=device, half=half, client_workers=client_worker)],
             TritonServerSpec(),
             dataset=None,
+            model_local_path=model_local_path[idx],
+            task=task[idx]
         ).run()
+
+if __name__ == "__main__":
+    mlperf()
