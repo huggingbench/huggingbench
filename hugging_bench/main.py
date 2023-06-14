53cff7f23f4f894540051f886f7b745cd4492bf0
kiarash <kiarash.rezahanjani@gmail.com>
2023-06-14 19:56:33 +0200

    Add ability to run experiments based on valid catresian product of given parameters

--- hugging_bench/main.py
+++ hugging_bench/main.py
@@ -1,6 +1,9 @@
 from hugging_bench.hugging_bench_config import ExperimentSpec, TritonServerSpec
 from hugging_bench.hugging_bench_runner import ExperimentRunner
 import argparse
+import logging
+
+LOG = logging.getLogger("CLI")
 
 
 def mlperf():
@@ -8,11 +11,15 @@ def mlperf():
     parser = argparse.ArgumentParser(description="runbench options")
 
     # Define the command-line arguments with their default values
-    parser.add_argument("--format", default="onnx", choices=["onnx", "trt", "openvino"])
-    parser.add_argument("--device", default="cpu", choices=["cpu", "cuda"])
-    parser.add_argument("--half", default=False, type=bool, help="Whether to use half precision")
+    parser.add_argument("--format", default=["onnx"], nargs="*", choices=["onnx", "trt", "openvino"])
+    parser.add_argument("--device", default=["cpu"], nargs="*", choices=["cpu", "cuda"])
+    parser.add_argument("--half", default=[False, True], nargs="*", type=bool, help="Whether to use half precision")
     parser.add_argument(
-        "--client_worker", default=1, type=int, help="Number of client workers sending concurrent requests to Triton"
+        "--client_worker",
+        default=[1],
+        nargs="*",
+        type=int,
+        help="Number of client workers sending concurrent requests to Triton",
     )
     parser.add_argument(
         "--hf_ids", default=["prajjwal1/bert-tiny"], nargs="*", help="HuggingFace model ID(s) to benchmark"
@@ -33,18 +40,30 @@ def mlperf():
     args = parser.parse_args()
 
     # Store the arguments in variables of the same name
-    format_type = args.format
-    device = args.device
+    format_types = args.format
+    devices = args.device
     half = args.half
-    client_worker = args.client_worker
+    client_workers = args.client_worker
     hf_ids = args.hf_ids
     model_local_path = args.model_local_path
     task = args.task
 
+    experiments = []
+    for f in format_types:
+        for d in devices:
+            for h in half:
+                for w in client_workers:
+                    experiment = ExperimentSpec(format=f, device=d, half=h, client_workers=w)
+                    if experiment.is_valid():
+                        LOG.info(f"Adding valid experiment: {experiment}")
+                        experiments.append(ExperimentSpec(format=f, device=d, half=h, client_workers=w))
+                    else:
+                        LOG.info(f"Skipping invalid experiment: {experiment}")
+
     for idx, hf_id in enumerate(hf_ids):
         ExperimentRunner(
             hf_id,
-            [ExperimentSpec(format=format_type, device=device, half=half, client_workers=client_worker)],
+            experiments,
             TritonServerSpec(),
             dataset=None,
             model_local_path=model_local_path[idx] if model_local_path else None,
