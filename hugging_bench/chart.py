f04c27c7738857e6c80f54834edd898364b6b71e
kiarash <kiarash.rezahanjani@gmail.com>
2023-06-15 12:37:29 +0200

    Add throughput to charts

--- hugging_bench/chart.py
+++ hugging_bench/chart.py
@@ -12,11 +12,13 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
     median_latencies = df["median"]
     percentile90_latencies = df["90_percentile"]
     percentile99_latencies = df["99_percentile"]
+    throughputs = df["success_rate"]
 
     # Set the figure size
     fig1, ax1 = plt.subplots(figsize=(12, 6))
     fig2, ax2 = plt.subplots(figsize=(12, 6))
     fig3, ax3 = plt.subplots(figsize=(12, 6))
+    fig4, ax4 = plt.subplots(figsize=(12, 6))
 
     # Create the chart for median latencies
     x_ticks = range(len(median_latencies))
@@ -30,7 +32,7 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
         ax1.text(
             i,
             median_latencies[i],
-            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch_size={label[3]}\nseq_len={label[4]}\ncli-worker={label[5]}\nasync_cli={label[6]}\nsuccess_rate={success_rate}",
+            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch={label[3]}\nseq={label[4]}\ncli={label[5]}\nasync={label[6]}\nsucc-rate={success_rate}",
             ha="center",
             va="bottom",
             fontsize=8,
@@ -51,7 +53,7 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
         ax2.text(
             i,
             percentile90_latencies[i],
-            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch={label[3]}\nseq_len={label[4]}\ncli_workers={label[5]}\nasync_cli={label[6]}\nsuccess_rate={success_rate}",
+            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch={label[3]}\nseq={label[4]}\ncli={label[5]}\nasync={label[6]}\nsucc-rate={success_rate}",
             ha="center",
             va="bottom",
             fontsize=8,
@@ -72,7 +74,7 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
         ax3.text(
             i,
             percentile99_latencies[i],
-            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch_size={label[3]}\nsequence_length={label[4]}\nclient_workers={label[5]}\nasync_clients={label[6]}\nsuccess_rate={success_rate}",
+            f"{label[0]}\n{label[1]}\nhalf={label[2]}\nbatch={label[3]}\nseq={label[4]}\ncli={label[5]}\nasync={label[6]}\nsucc-rate={success_rate}",
             ha="center",
             va="bottom",
             fontsize=8,
@@ -82,6 +84,27 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
     ax3.set_ylabel("99th Percentile Latency")
     ax3.set_title("Comparison of 99th Percentile Latencies", loc="left")
 
+    # Create the chart for throughput
+    ax4.bar(x_ticks, throughputs)
+    ax4.set_xticks(x_ticks)
+    ax4.set_xticklabels([])  # Remove x-axis labels
+
+    # Add x-axis labels inside the bars
+    for i, label in enumerate(labels.values):
+        p99 = "{:.4f}".format(percentile99_latencies[i])
+        ax4.text(
+            i,
+            throughputs[i],
+            f"{label[0]}\n{label[1]}\np99={p99}\nhalf={label[2]}\nbatch={label[3]}\nseq={label[4]}\ncli={label[5]}\nasync={label[6]}",
+            ha="center",
+            va="bottom",
+            fontsize=7,
+        )
+
+    ax4.set_xlabel("Configuration")
+    ax4.set_ylabel("throughput")
+    ax4.set_title("Throughput", loc="left")
+
     # Adjust spacing between subplots and ensure labels fit
     plt.tight_layout()
 
@@ -89,11 +112,13 @@ def create_charts(stats_path="temp/prajjwal1-bert-tiny.csv"):
     fig1.savefig("median_latencies.png", dpi=300)
     fig2.savefig("percentile90_latencies.png", dpi=300)
     fig3.savefig("percentile99_latencies.png", dpi=300)
+    fig4.savefig("throughputs.png", dpi=300)
 
     # Close the plots
     plt.close(fig1)
     plt.close(fig2)
     plt.close(fig3)
+    plt.close(fig4)
 
 
 if __name__ == "__main__":
