5068de882d09ed5d603073df935408c8c63fd9a4
kiarash <kiarash.rezahanjani@gmail.com>
2023-06-19 15:23:19 +0200

    Undo unintended commenting

--- test/test_e2e_experiment_runner.py
+++ test/test_e2e_experiment_runner.py
@@ -16,9 +16,9 @@ def test_experiment_runner():
         hf_id="prajjwal1/bert-tiny", format="onnx", device="cpu", half=False, batch_size=1
     )  # given model only supports batch size 1
     experiment_runner = ExperimentRunner([experiment], server_spec)
-    # os.environ[
-    #     ENV_TRITON_SERVER_DOCKER
-    # ] = "ghcr.io/niksajakovljevic/tritonserver:23.04-onnx"  # we use custom docker to reduce image size
+    os.environ[
+        ENV_TRITON_SERVER_DOCKER
+    ] = "ghcr.io/niksajakovljevic/tritonserver:23.04-onnx"  # we use custom docker to reduce image size
     experiment_runner.run()
     csv_file = Path(experiment.get_csv_output_path())
     if not csv_file.exists():
