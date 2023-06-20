70d3f9b99ac985e81f7e47f9f1bfe4df02180060
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-06-20 16:43:48 +0200

    Fix e2e test

--- test/test_e2e_experiment_runner.py
+++ test/test_e2e_experiment_runner.py
@@ -1,3 +1,4 @@
+import os
 from gevent import monkey
 
 monkey.patch_all()  # this is needed to make gevent work with pytest
@@ -5,17 +6,18 @@ from pathlib import Path
 
 from bench.config import ExperimentSpec
 from bench.runner import ExperimentRunner
+from server.util import ENV_TRITON_SERVER_DOCKER
 
 
 def test_experiment_runner():
     experiment = ExperimentSpec(
         hf_id="prajjwal1/bert-tiny", format="onnx", device="cpu", half=False, batch_size=1
     )  # given model only supports batch size 1
+    os.environ[
+        ENV_TRITON_SERVER_DOCKER
+    ] = "ghcr.io/niksajakovljevic/tritonserver:23.04-onnx"  # we use custom docker to reduce image size
     ExperimentRunner([experiment], workspace_dir="./temp").run()
-    # os.environ[
-    #     ENV_TRITON_SERVER_DOCKER
-    # ] = "ghcr.io/niksajakovljevic/tritonserver:23.04-onnx"  # we use custom docker to reduce image size
-    # experiment_runner.run()
+
     csv_file = Path(experiment.get_csv_output_path("./temp"))
     if not csv_file.exists():
         assert False, "CSV file not generated"
