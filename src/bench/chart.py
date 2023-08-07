3f72e35a9d70999c86ecd01d01b00e495d167a49
kiarash <kiarash.rezahanjani@gmail.com>
2023-08-07 08:28:48 -0400

    Single chart of throughout and latency

--- src/bench/chart.py
+++ src/bench/chart.py
@@ -94,7 +94,61 @@ class ChartGen:
             self.plot_chart(labels, chart_data, chart_name, output_dir, model_id)
 
 
+    def single_chart(self, df, file_path: str = "latency_throughput.png"):
+        fig, ax = plt.subplots(1, 2, figsize=(20,10))
+
+        # Create a column that represents each unique combination of 'format' and 'device'
+        df['format_device'] = df['format'].astype(str) + '_' + df['device'].astype(str)
+
+        # Create a label column for each bar
+        df['bar_label'] = '[batch:' + df['batch_size'].astype(str) + ',seq:' + df['sequence_length'].astype(str) + ',instances:' + df['instances'].astype(str) + ']'
+
+        # sort the data
+        df_sorted_throughput = df.sort_values('throughput', ascending=False)
+        df_sorted_90 = df.sort_values('90_percentile', ascending=True)
+
+        # create a color palette
+        palette = dict(zip(df['format_device'].unique(), sns.color_palette('hls', n_colors=df['format_device'].nunique())))
+
+        # horizontal bar plot for throughput
+        sns.barplot(y=df_sorted_throughput.index, x='throughput', hue='format_device', data=df_sorted_throughput, 
+                    palette=palette, ax=ax[0], orient='h')
+        ax[0].set_title('Throughput')
+
+        # add value annotations for throughput
+        xt = df_sorted_throughput.throughput.max() / 2
+        for i in range(df_sorted_throughput.shape[0]):
+            ax[0].text(10, i, 
+                    f'{df_sorted_throughput.throughput.iloc[i]:.2f} rps', 
+                    color='black', ha="left", va="center", fontsize=8)
+            ax[0].text(xt, i, 
+                    df_sorted_throughput.bar_label.iloc[i], 
+                    color='black', ha="left", va="center", fontsize=8)
+
+        # horizontal bar plot for 90_percentile
+        sns.barplot(y=df_sorted_90.index, x='90_percentile', hue='format_device', data=df_sorted_90, 
+                    palette=palette, ax=ax[1], orient='h')
+        ax[1].set_title('90_percentile')
+
+        # add value annotations for 90_percentile
+        x90 = df_sorted_90["90_percentile"].max() / 2
+        for i in range(df_sorted_90.shape[0]):
+            ax[1].text(10, i, 
+                    f'{(df_sorted_90["90_percentile"].iloc[i] * 1000):.2f} ms', 
+                    color='black', ha="left", va="center", fontsize=8)
+            ax[1].text(x90, i, 
+                    df_sorted_90.bar_label.iloc[i], 
+                    color='black', ha="left", va="center", fontsize=8)
+
+        plt.tight_layout()
+        fig.savefig(file_path)
+        LOG.info(f"Saved chart to '{file_path}'")
+        plt.close(fig)
+
+
 if __name__ == "__main__":
     chart_gen = ChartGen()
     chart_gen.add_data(pd.read_csv("./temp/tmp/test.csv"))
     chart_gen.plot_charts(model_id="prajjwal1/bert-tiny", df=None, output_dir="./temp/tmp/")
+    # single chart of throughput and latency
+    chart_gen.single_chart(pd.read_csv("./temp/tmp/test.csv"))
